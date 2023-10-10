637f2ce5344b2b8188e8342b1079aee659c2c3b8
William05Cardozo <91096606+William05Cardozo@users.noreply.github.com>
2023-10-09 20:29:01 -0500

    Database script

---
+++ RCPiscinas_DB.sql
@@ -0,0 +1,15 @@
+use Productos;
+CREATE TABLE productos (
+id int,
+Categoria varchar(200),
+Subcategoria varchar(200),
+Linea varchar(200),
+Nombre varchar(200),
+Precio int,
+Descripción varchar(200),
+Codigo int,
+PRIMARY KEY (id)
+);
+ALTER TABLE productos MODIFY COLUMN id int auto_increment;
+INSERT INTO productos (Categoria, Subcategoria, Linea) VALUES ('Piscina de fibra', 'Cascos', 'Doble banco');
+SELECT * FROM productos;
